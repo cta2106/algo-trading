fixed SR calculation for buy/hold

intraday_trend/src/metrics.py
@@ -2,5 +2,5 @@ import numpy as np
 import pandas as pd
 
 
-def sharpe_ratio(df_pnl: pd.DataFrame) -> float:
-    return (252 ** 0.5) * np.mean(df_pnl["pnl"]) / np.std(df_pnl["pnl"])
+def sharpe_ratio(df_in: pd.DataFrame) -> float:
+    return (252 ** 0.5) * np.mean(df_in["pnl"]) / np.std(df_in["pnl"])